2ca7c08ee94efda8da687bf4aaee33d76b348575
akashbhardwaj23 <akashbhardwaj415@gmail.com>
2025-08-07 17:47:39 +0100

    added logger on api routes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger';
 
 const app = new Hono()
 
-const welcomeStrings = [
-  "Hello world"
-]
+app.use('/api/*', logger());
 
 app.get('/', (c) => {
-  return c.text(welcomeStrings.join('\n\n'))
+  return c.json({
+    message : "hello"
+  })
 })
 
 export default app
