e4fd6b076297d9a473b9423199b6413d3b15c8e1
akashbhardwaj23 <akashbhardwaj415@gmail.com>
2025-08-07 18:02:15 +0100

    added zod schema

---
+++ src/zod/type.ts
@@ -0,0 +1,10 @@
+import {z} from "zod"
+
+
+export const SubscriptionSchema = z.object({
+    userId : z.string(),
+    newPlan : z.enum(['monthly_spiritual', 'annual_spiritual', 'free']),
+    effectiveDate : z.string().refine(date => !isNaN(Date.parse(date)), {
+      message: "Effective date must be a valid date string"
+    })
+})
